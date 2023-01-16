f889cd0e305399c8a56689eb3fe5ef6acf859367
zeeshanmnawaz786 <zeeshanm.nawaz786@gmail.com>
2023-01-16 22:03:21 +0500

    simpleCalculator in react

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const inputRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    setResult(result + Number(inputRef.current.value));
+  }
+
+  function minus(e) {
+    e.preventDefault();
+    setResult(result - Number(inputRef.current.value));
+    // Add the code for the minus function
+  }
+
+  function times(e) {
+    e.preventDefault();
+    setResult(result * Number(inputRef.current.value));
+    // Add the code for the plus function
+  }
+
+  function divide(e) {
+    e.preventDefault();
+    setResult(result / Number(inputRef.current.value));
+    // Add the code for the divide function
+  }
+
+  function resetInput(e) {
+    e.preventDefault();
+    inputRef.current.value = null;
+    // Add the code for the resetInput function
+  }
+
+  function resetResult(e) {
+    e.preventDefault();
+    setResult(0);
+    // Add the code for the resetResult function
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p>{result}</p>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>add</button>
+        {/* Add the subtract button */}
+        <button onClick={minus}>Substract</button>
+        {/* Add the multiply button */}
+        <button onClick={times}>multipyr</button>
+        {/* Add the divide button */}
+        <button onClick={divide}>Divide</button>
+        {/* Add the resetInput button */}
+        <button onClick={resetInput}>Reset Input</button>
+        {/* Add the resetResult button */}
+        <button onClick={resetResult}>Reset Input</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
